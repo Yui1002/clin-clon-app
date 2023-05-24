0ba593130fa434c231a43ab1e914364d7646e4a1
Yui Murayama <yuimurayama1002@gmail.com>
2023-05-23 21:38:12 -0700

    display a list of records on history page

--- components/History.tsx
+++ components/History.tsx
@@ -1,20 +1,29 @@
-import {View, Button, Text, FlatList, AsyncStorage} from 'react-native';
-import React, {useEffect} from 'react';
+import {View, Button, Text, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
 type Props = {};
 
+interface RecordData {
+  record: string;
+}
+
 const History = () => {
+  const [records, setRecords] = useState<string | null>(null);
+
   useEffect(() => {
-    async function fetchData() {
-      const response = await AsyncStorage.getItem('records');
-      console.log(response);
-    }
-    fetchData();
-  }, []);
+    AsyncStorage.getItem('records', (_err, result) => {
+      if (!result) {
+        setRecords('no record');
+      } else {
+        setRecords(JSON.parse(result));
+      }
+    });
+  }, [records]);
 
   return (
     <View>
-      <FlatList />
+      <FlatList data={records} renderItem={({item}) => <Text>{item}</Text>} />
     </View>
   );
 };
